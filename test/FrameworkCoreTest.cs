c5ac737b09b1324674ffb50fa16b8327b51913f9
Randy <361703739@qq.com>
2016-12-28 01:38:52 +0800

    xunit
    use xunit for test

---
+++ test/FrameworkCoreTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace Tests
+{
+    public class FrameworkCoreTest
+    {
+        [Fact]
+        public void Test1() 
+        {
+            Assert.True(true);
+        }
+    }
+}
